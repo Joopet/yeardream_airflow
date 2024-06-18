c98ef6a2dbe3c04f883647ba56f09fb74d90afff
Joopet <xjoxonx@gmail.com>
2024-06-18 16:56:11 +0900

    DAG 만들어보기 실습 수정4

--- dags/ex_dags_bash_operator_decorator.py
+++ dags/ex_dags_bash_operator_decorator.py
@@ -1,4 +1,4 @@
-from airflow import DAG
+from airflow.decorators import dag
 import datetime
 import pendulum
 from airflow.operators.bash import BashOperator
@@ -8,7 +8,8 @@ from airflow.operators.bash import BashOperator
     schedule="0 13 * * 5#2",
     start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
     catchup=False,
-    tags=["homework"])
+    tags=["homework"],
+    )
 def generate_dag():
     bash_t1 = BashOperator(
         task_id="bash_t1",
