5976093ce7ce74786903d044e0439beb9c71873c
Joopet <xjoxonx@gmail.com>
2024-06-21 15:13:43 +0900

    dags_email_on_failure

--- dags/dags_email_on_failure.py
+++ dags/dags_email_on_failure.py
@@ -1,18 +1,36 @@
-from airflow import Dataset
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.decorators import task
+from airflow.exceptions import AirflowException
 import pendulum
+from datetime import timedelta
+from airflow.models import Variable
 
-dataset_dags_dataset_producer_2 = Dataset("dags_dataset_producer_2")
+email_str = Variable.get("email_target")
+email_lst = [email.strip() for email in email_str.split(',')]
 
 with DAG(
-        dag_id='dags_dataset_producer_2',
-        schedule='0 7 * * *',
-        start_date=pendulum.datetime(2024, 6, 16, tz='Asia/Seoul'),
-        catchup=False
+    dag_id='dags_email_on_failure',
+    start_date=pendulum.datetime(2024,6,16, tz='Asia/Seoul'),
+    catchup=False,
+    schedule='0 1 * * *',
+    dagrun_timeout=timedelta(minutes=2),
+    default_args={
+        'email_on_failure': True,
+        'email': email_lst
+    }
 ) as dag:
-    bash_task = BashOperator(
-        task_id='bash_task',
-        outlets=[dataset_dags_dataset_producer_2],
-        bash_command='echo "producer_2 수행 완료"'
+    @task(task_id='python_fail')
+    def python_task_func():
+        raise AirflowException('에러 발생')
+    python_task_func()
+
+    bash_fail = BashOperator(
+        task_id='bash_fail',
+        bash_command='exit 1',
+    )
+
+    bash_success = BashOperator(
+        task_id='bash_success',
+        bash_command='exit 0',
     )
